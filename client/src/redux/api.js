9c9c5e1152160fdbb66811f7263cf23187d3ae29
prabhajaiswal <114211205+prabhajaiswal@users.noreply.github.com>
2023-12-01 20:35:40 +0530

    Add modified files and .gitignore

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const devEnv = process.env.NODE_ENV !== "production";
 
-const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
+//const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
 const API = axios.create({
-  baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
+  baseURL: `${devEnv ? "http://localhost:5000" : "https://touropedia.herokuapp.com/"}`,
 });
 
 API.interceptors.request.use((req) => {
